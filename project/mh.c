3487254acc0e7ed9e842551b32e54f33bc785360
tfohrmann <55881099+tfohrmann@users.noreply.github.com>
2020-03-16 20:54:48 +0100

    new action

--- project/mh.c
+++ project/mh.c
@@ -11,21 +11,20 @@
 #include <gsl/gsl_rng.h>
 #include <sys/time.h>
 
-double delta_s(double**** lattice, int T, int N, double change, int pos[4], double mass, double field, double zeta);
-double sweep(double**** lattice, int T, int N, double mass, double field, double zeta, double w);
+double delta_s(double**** lattice, int T, int N, double change, int pos[4], double mass, double field);
+double sweep(double**** lattice, int T, int N, double mass, double field, double w);
 void save_to_txt(double**** lattice, int T, int N, char* name);
 
 int main(){
     // Simulation parameters
     const double mass = 1.0;
     const double field = 0.0;
-    const double zeta = 1.0;
     
     const int N = 16; //size of lattice in "time-direction"
     const int T = 32; //size of lattice in "space-direction"
     
     const double w = 1.0; //width of (uniform) proposal density
-    const int n_sweeps = 100; //number of sweeps to be calculated in total
+    const int n_sweeps = 30000; //number of sweeps to be calculated in total
     
     
     // Random generator initilisation
@@ -84,7 +83,8 @@ int main(){
 			{
                 for(int l=0; l<N; l++)
                 {
-                    lattice[i][j][k][l] /= total;
+                    //lattice[i][j][k][l] /= total;
+                    lattice[i][j][k][l] = 0;
                 }	
 			}
 		}
@@ -94,16 +94,18 @@ int main(){
     /*
     // Thermalise
     for(int i=0; i<100; i++){
-        AR += sweep(lattice, T, N, mass, field, zeta, w);
+        AR += sweep(lattice, T, N, mass, field, w);
     }
     printf("Thermalised!\n");
     */
     // Perform n_sweeps sweeps of the lattice
     char name[30];
     for(int i=0; i<n_sweeps; i++){
-        AR += sweep(lattice, T, N, mass, field, zeta, w);
-        sprintf(name, "./m1f0z1/sweep_%d.txt", i+1);
-        save_to_txt(lattice, T, N, name);
+        AR += sweep(lattice, T, N, mass, field, w);
+        if(i%100==0){
+            sprintf(name, "./m1f0/sweep_%d.txt", i/100);
+            save_to_txt(lattice, T, N, name);
+        }
         
         // Fortschrittsanzeige
 		printf("\b\b\b\b%3.0lf%%", ((double)i)/((double)n_sweeps)*100.0);
@@ -119,60 +121,58 @@ int main(){
 }
 
 
-double delta_s(double**** lattice, int T, int N, double change, int pos[4], double mass, double field, double zeta){
+double delta_s(double**** lattice, int T, int N, double change, int pos[4], double mass, double field){
     // Calculates the difference in the action given a proposed change to the lattice at position pos.
     double delta = 0;
     double sp = lattice[pos[0]][pos[1]][pos[2]][pos[3]] + change;
     
     // After change:
     // Time
-    double a = 2.0*mass*zeta;
-    delta += -a * sp * lattice[(pos[0]-1+T)%T][pos[1]][pos[2]][pos[3]]; //before
-    delta += -a * sp * lattice[(pos[0]+1)%T][pos[1]][pos[2]][pos[3]]; //after
+    delta += pow(sp - lattice[(pos[0]-1+T)%T][pos[1]][pos[2]][pos[3]], 2) /2.0; //before
+    delta += pow(lattice[(pos[0]+1)%T][pos[1]][pos[2]][pos[3]] - sp, 2) /2.0; //after
     
     // x-Dim
-    double b = 2.0*mass/zeta;
-    delta += -b * sp * lattice[pos[0]][(pos[1]-1+N)%N][pos[2]][pos[3]]; //before
-    delta += -b * sp * lattice[pos[0]][(pos[1]+1)%N][pos[2]][pos[3]]; //after
+    delta += pow(sp - lattice[pos[0]][(pos[1]-1+N)%N][pos[2]][pos[3]], 2) /2.0; //before
+    delta += pow(lattice[pos[0]][(pos[1]+1)%N][pos[2]][pos[3]] - sp, 2) /2.0; //after
     
     // y-Dim
-    delta += -b * sp * lattice[pos[0]][pos[1]][(pos[2]-1+N)%N][pos[3]]; //before
-    delta += -b * sp * lattice[pos[0]][pos[1]][(pos[2]+1)%N][pos[3]]; //after
+    delta += pow(sp - lattice[pos[0]][pos[1]][(pos[2]-1+N)%N][pos[3]], 2) /2.0; //before
+    delta += pow(lattice[pos[0]][pos[1]][(pos[2]+1)%N][pos[3]] - sp, 2) /2.0; //after
     
     // z-Dim
-    delta += -b * sp * lattice[pos[0]][pos[1]][pos[2]][(pos[3]-1+N)%N]; //before
-    delta += -b * sp * lattice[pos[0]][pos[1]][pos[2]][(pos[3]+1)%N]; //after
+    delta += pow(sp - lattice[pos[0]][pos[1]][pos[2]][(pos[3]-1+N)%N], 2) /2.0; //before
+    delta += pow(lattice[pos[0]][pos[1]][pos[2]][(pos[3]+1)%N] - sp, 2) /2.0; //after
     
     // Pure Field
-    delta += (1.0-2.0*field)*pow(sp,2) + field*pow(sp,4);
+    delta += -pow(mass * sp, 2)/2.0 - field/24.0 * pow(sp, 4);
     
     
     // Before change:
     sp -= change;
     // Time
-    delta -= -a * sp * lattice[(pos[0]-1+T)%T][pos[1]][pos[2]][pos[3]]; //before
-    delta -= -a * sp * lattice[(pos[0]+1)%T][pos[1]][pos[2]][pos[3]]; //after
+    delta -= pow(sp - lattice[(pos[0]-1+T)%T][pos[1]][pos[2]][pos[3]], 2) /2.0; //before
+    delta -= pow(lattice[(pos[0]+1)%T][pos[1]][pos[2]][pos[3]] - sp, 2) /2.0; //after
     
     // x-Dim
-    delta -= -b * sp * lattice[pos[0]][(pos[1]-1+N)%N][pos[2]][pos[3]]; //before
-    delta -= -b * sp * lattice[pos[0]][(pos[1]+1)%N][pos[2]][pos[3]]; //after
+    delta -= pow(sp - lattice[pos[0]][(pos[1]-1+N)%N][pos[2]][pos[3]], 2) /2.0; //before
+    delta -= pow(lattice[pos[0]][(pos[1]+1)%N][pos[2]][pos[3]] - sp, 2) /2.0; //after
     
     // y-Dim
-    delta -= -b * sp * lattice[pos[0]][pos[1]][(pos[2]-1+N)%N][pos[3]]; //before
-    delta -= -b * sp * lattice[pos[0]][pos[1]][(pos[2]+1)%N][pos[3]]; //after
+    delta -= pow(sp - lattice[pos[0]][pos[1]][(pos[2]-1+N)%N][pos[3]], 2) /2.0; //before
+    delta -= pow(lattice[pos[0]][pos[1]][(pos[2]+1)%N][pos[3]] - sp, 2) /2.0; //after
     
     // z-Dim
-    delta -= -b * sp * lattice[pos[0]][pos[1]][pos[2]][(pos[3]-1+N)%N]; //before
-    delta -= -b * sp * lattice[pos[0]][pos[1]][pos[2]][(pos[3]+1)%N]; //after
+    delta -= pow(sp - lattice[pos[0]][pos[1]][pos[2]][(pos[3]-1+N)%N], 2) /2.0; //before
+    delta -= pow(lattice[pos[0]][pos[1]][pos[2]][(pos[3]+1)%N] - sp, 2) /2.0; //after
     
     // Pure Field
-    delta -= (1.0-2.0*field)*pow(sp,2) + field*pow(sp,4);
+    delta -= -pow(mass * sp, 2)/2.0 - field/24.0 * pow(sp, 4);
     
     
     return delta;
 }
 
-double sweep(double**** lattice, int T, int N, double mass, double field, double zeta, double w){
+double sweep(double**** lattice, int T, int N, double mass, double field, double w){
     // Sequentially makes update to the entire lattice.
     // Random generator init
     const gsl_rng_type * type;
@@ -192,6 +192,7 @@ double sweep(double**** lattice, int T, int N, double mass, double field, double
     double proposal;
     double del_S;
     int pos[4];
+    double norm = 1.0/pow(mass, 4);
     for(int i=0; i<T;  i++)
 	{
         pos[0] = i;
@@ -205,9 +206,9 @@ double sweep(double**** lattice, int T, int N, double mass, double field, double
                 {
                     pos[3] = l;
                     proposal = w * gsl_rng_uniform(s) - w/2.0;
-                    del_S = delta_s(lattice, T, N, proposal, pos, mass, field, zeta);
+                    del_S = delta_s(lattice, T, N, proposal, pos, mass, field);
                     
-                    if(gsl_rng_uniform(s) < exp(-del_S)){
+                    if(gsl_rng_uniform(s) < exp(-del_S*norm)){
                         lattice[i][j][k][l] += proposal;
                         AR += 1.0;
                     }
